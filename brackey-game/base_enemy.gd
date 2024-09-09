c4322885645289e0886c2657cf49543a8190f95f
Kai Phosy <61040372+KaiPhosy@users.noreply.github.com>
2024-09-09 11:24:26 -0700

    added base_enemy: health, hit signal, hit animation

--- brackey-game/base_enemy.gd
+++ brackey-game/base_enemy.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+signal hit
+
+@export var health = 100 
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,7 +13,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	# example https://forum.godotengine.org/t/want-enemy-to-play-hurt-animation-when-hit/59639/3
+	if $AnimatedSprite2D.animation == "hit" and $AnimatedSprite2D.is_playing():
+		pass
+	elif $AnimatedSprite2D.animation != "default":
+		print('playing default animation')
+		$AnimatedSprite2D.play("default")
+		
 
 
 func _on_area_entered(area: Area2D) -> void:
 	print("enemy was hit")
+	hit.emit()
+	$AnimatedSprite2D.play("hit")
