f23dd207f4ade2da154047744687740266c05a18
Kai Phosy <61040372+KaiPhosy@users.noreply.github.com>
2024-09-12 14:52:14 -0700

    completed bullet turret - currently uses player bullet pen+dmg

--- brackey-game/bullet_turret.gd
+++ brackey-game/bullet_turret.gd
@@ -12,24 +12,44 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if can_shoot:
+		shoot()
 
 
 func shoot():
-	if can_shoot:
-		can_shoot = false
-		# Wait
-		$BulletTimer.set_wait_time(1)
-		$BulletTimer.start()
-		create_bullet(0)
+	can_shoot = false
+	# Wait
+	$BulletTimer.set_wait_time(1)
+	$BulletTimer.start()
+	var enemy_position = aim()
+	if enemy_position:
+		create_bullet(enemy_position)
 
-func create_bullet(offset):
+func create_bullet(enemy_position):
 	var b = bullet_scene.instantiate()
 	get_tree().root.add_child(b)
-	b.start_direction(self.global_position, self.global_position.direction_to(get_global_mouse_position()).rotated(offset))
+	b.start_direction(self.global_position, self.global_position.direction_to(enemy_position))
 	
+func aim():
+	var in_range = get_overlapping_areas()
+	
+	var nearest_d_ind = -1
+	var nearest_d_squared: float = -1
+	if in_range:
+		for i in range (in_range.size()):
+			print(i)
+			if in_range[i].is_in_group("enemy"):
+				var d_squared = self.global_position.distance_squared_to(in_range[i].global_position)
+				if nearest_d_squared == -1 or d_squared <= nearest_d_squared:
+					nearest_d_squared = d_squared
+					nearest_d_ind = i
+		
+	if nearest_d_ind == -1:
+		return false
+	else:
+		return in_range[nearest_d_ind].global_position
 
-
-
+	
+	
 func _on_bullet_timer_timeout() -> void:
 	can_shoot = true
